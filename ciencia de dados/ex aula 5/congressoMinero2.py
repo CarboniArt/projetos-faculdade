8faedb228d43892e424aa7a8e19d7fd8f297f9aa
Arthur V. C. Linzing <139267313+CarboniArt@users.noreply.github.com>
2026-03-23 11:26:42 -0300

    att ciencia de dados

---
+++ ciencia de dados/ex aula 5/congressoMinero2.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+url = ("https://www.house.gov/representatives")
+
+text = requests.get(url).text
+
+soup = BeautifulSoup(text, 'html5lib')
+
+all_urls = [a['href']
+            for a in soup('a')
+            if a.has_attr('href')]
+
+print (len(all_urls))
+
+
+regex = r'^https?://.*\.house\.gov/?$'
+
+assert re.match(regex, "https://joel.house.gov")
+
+assert re.match(regex, "https://joel.house.gov/")
+
+assert not re.match(regex, "https://joel.house.com")
+
+assert not re.match(regex, "https://joel.house.gov/biography")
+
+good_urls = [url for url in all_urls if re.match(regex, url)]
+
+good_urls = list(set(good_urls))
+
+print(len(good_urls))
